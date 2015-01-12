c2baff656b1bc09872729aeb6700d6439907e7bc
unknown <afogelbe@CZC2393PJG.adm.enkoping.se>
2015-01-12 16:06:39 +0100

    Updated copyright

--- src/Popup.js
+++ src/Popup.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Copyright 2014 Mälardalskartan
+ * Copyright 2015 Mälardalskartan
  * Licensed under BSD 2-Clause (https://github.com/malardalskartan/mdk/blob/master/LICENSE.txt)
  * ======================================================================== */
 
