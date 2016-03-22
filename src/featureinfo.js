8f8e7496737d71e167dc790c5db1e39b12fe2a66
afogelberg <andreas.fogelberg@vasteras.se>
2016-03-22 13:53:05 +0100

    Added featureinfo templates

--- src/featureinfo.js
+++ src/featureinfo.js
@@ -10,6 +10,7 @@ var Viewer = require('./viewer');
 var Popup = require('./popup');
 var sidebar = require('./sidebar');
 var maputils = require('./maputils');
+var featureinfotemplates = require('./featureinfotemplates');
 var owlCarousel = require('../externs/owlcarousel-browserify');
 owlCarousel.loadjQueryPlugin();
 
@@ -249,53 +250,74 @@ module.exports = function(options) {
       var attribute, li = '', title, val;
       //If layer is configured with attributes
       if(layer.get('attributes')) {
-        for(var i=0; i<layer.get('attributes').length; i++) {
-          attribute = layer.get('attributes')[i];
-          title = '';
-          val = '';
-          if (attribute['name']) {
-            if(feature.get(attribute['name'])) {
-                val = feature.get(attribute['name']);
-                if (attribute['title']) {
-                  title = '<b>' + attribute['title'] + '</b>';
-                }
-                if (attribute['url']) {
-                  var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], feature.get(attribute['url']));
-                  val = '<a href="' + url + '" target="_blank">' +
-                        feature.get(attribute['name']) +
-                        '</a>';
-                }
+            //If attributes is string then use template named with the string
+            if(typeof layer.get('attributes') === 'string') {
+                //Clean feature attributes from non-wanted properties
+                var attributes = filterObject(feature.getProperties(), ['FID_', 'geometry']);
+                //Use attributes with the template
+                li = featureinfotemplates[layer.get('attributes')](attributes);
             }
-          }
-          else if (attribute['url']) {
-              if(feature.get(attribute['url'])) {
-                  var text = attribute['html'] || attribute['url'];
-                  var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], feature.get(attribute['url']));
-                  val = '<a href="' + url + '" target="_blank">' +
-                        text +
-                        '</a>';
-              }
-          }
-          else if (attribute['img']) {
-              if(feature.get(attribute['img'])) {
-                  var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], feature.get(attribute['img']));
-                  var attribution = attribute['attribution'] ? '<div class="image-attribution">' + attribute['attribution'] + '</div>' : '';
-                  val = '<div class="image-container">' +
-                            '<img src="' + url + '">' + attribution +
-                        '</div>';
-              }
-          }
-          else if (attribute['html']) {
-            val = attribute['html'];
-          }
+            else {
+                for(var i=0; i<layer.get('attributes').length; i++) {
+                  attribute = layer.get('attributes')[i];
+                  title = '';
+                  val = '';
+                  if (attribute['name']) {
+                    if(feature.get(attribute['name'])) {
+                        val = feature.get(attribute['name']);
+                        if (attribute['title']) {
+                          title = '<b>' + attribute['title'] + '</b>';
+                        }
+                        if (attribute['url']) {
+                          var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], feature.get(attribute['url']));
+                          val = '<a href="' + url + '" target="_blank">' +
+                                feature.get(attribute['name']) +
+                                '</a>';
+                        }
+                    }
+                  }
+                  else if (attribute['url']) {
+                      if(feature.get(attribute['url'])) {
+                          var text = attribute['html'] || attribute['url'];
+                          var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], feature.get(attribute['url']));
+                          val = '<a href="' + url + '" target="_blank">' +
+                                text +
+                                '</a>';
+                      }
+                  }
+                  else if (attribute['img']) {
+                      if(feature.get(attribute['img'])) {
+                          var url = createUrl(attribute['urlPrefix'], attribute['urlSuffix'], feature.get(attribute['img']));
+                          var attribution = attribute['attribution'] ? '<div class="image-attribution">' + attribute['attribution'] + '</div>' : '';
+                          val = '<div class="image-container">' +
+                                    '<img src="' + url + '">' + attribution +
+                                '</div>';
+                      }
+                  }
+                  else if (attribute['html']) {
+                    val = attribute['html'];
+                  }
 
-          var cls = ' class="' + attribute['cls'] + '" ' || '';
+                  var cls = ' class="' + attribute['cls'] + '" ' || '';
 
-          li += '<li' + cls +'>' + title + val + '</li>';
-        }
+                  li += '<li' + cls +'>' + title + val + '</li>';
+                }
+          }
+      }
+      else {
+          li = featureinfotemplates.default({});
       }
       content += li + '</ul></div>';
       return content;
+    }
+    function filterObject(obj, excludedKeys) {
+        var result = {}, key;
+        for (key in obj) {
+            if(excludedKeys.indexOf(key) === -1) {
+                result[key] = obj[key];
+            }
+        }
+        return result;
     }
     function createUrl(prefix, suffix, url) {
         var p = prefix || '';
---
+++ src/featureinfotemplates.js
@@ -0,0 +1,10 @@
+/* ========================================================================
+ * Copyright 2016 Mälardalskartan
+ * Licensed under BSD 2-Clause (https://github.com/malardalskartan/mdk/blob/master/LICENSE.txt)
+ * ======================================================================== */
+"use strict";
+
+var featureinfotemplates = {};
+featureinfotemplates.default = require("./templates/featureinfotemplates/default.handlebars");
+
+module.exports = featureinfotemplates;
